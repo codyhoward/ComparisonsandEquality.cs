480950581b2689b227be499d246e9fa87d129c9b
close <close@BABYGIRL>
2016-11-10 00:46:38 -0500

    Multiple updates to this repo are unlikely -- the way the examples work in this course span across multiple applications that are very basic in their nature.

--- Food.cs/Program.cs
+++ Food.cs/Program.cs
@@ -10,6 +10,13 @@ namespace Food.cs
     {
         static void Main(string[] args)
         {
+            Food banana = new Food("banana");
+            Food banana2 = new Food("banana");
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));
         }
+
     }
 }
